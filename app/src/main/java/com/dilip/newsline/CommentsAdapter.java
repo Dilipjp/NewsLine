653396a7fd93b7446d87b826c425c4aa44460299
Dilipjp <jpdilipk@gmail.com>
2024-07-22 04:03:29 +0530

    User name added to comments

--- app/src/main/java/com/dilip/newsline/CommentsAdapter.java
+++ app/src/main/java/com/dilip/newsline/CommentsAdapter.java
@@ -1,20 +1,28 @@
 package com.dilip.newsline;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
 public class CommentsAdapter extends BaseAdapter {
     private Context context;
     private List<Comment> comments;
-    private DatabaseReference commentsDatabaseReference;
+    private DatabaseReference usersDatabaseReference;
 
     public CommentsAdapter(Context context, List<Comment> comments) {
         this.context = context;
@@ -42,11 +50,27 @@ public class CommentsAdapter extends BaseAdapter {
             convertView = LayoutInflater.from(context).inflate(R.layout.comment_list_item, parent, false);
         }
         final Comment comment = comments.get(position);
+        TextView tvUsername = convertView.findViewById(R.id.tvUserName);
         TextView tvComment = convertView.findViewById(R.id.tvComment);
 
         tvComment.setText(comment.getCommentText());
+        usersDatabaseReference = FirebaseDatabase.getInstance().getReference("users").child(comment.getCommentUserId());
+        usersDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String name = dataSnapshot.child("username").getValue(String.class);
+                    tvUsername.setText(name);
+                } else {
+                    Log.d(TAG, "User data not found in the database");
+                }
+            }
 
-
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.e(TAG, "Failed to read user data", error.toException());
+            }
+        });
         return convertView;
     }
 }
