7e047cf528e84de9a67c05af87604da19bd86f94
Dilipjp <jpdilipk@gmail.com>
2024-08-06 08:26:56 +0530

    dark/light mode created

--- app/src/main/java/com/dilip/newsline/SettingsActivity.java
+++ app/src/main/java/com/dilip/newsline/SettingsActivity.java
@@ -1,18 +1,65 @@
 package com.dilip.newsline;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private Switch switchDarkMode;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private Button btn_back;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        switchDarkMode = findViewById(R.id.switch_dark_mode);
+        btn_back = findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(view ->  {
+            Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        // Check current theme setting
+        boolean isDarkModeOn = sharedPreferences.getBoolean("isDarkModeOn", false);
+        switchDarkMode.setChecked(isDarkModeOn);
+
+        // Set theme based on saved preference
+        if (isDarkModeOn) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
+        // Listener for toggle switch
+        switchDarkMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor.putBoolean("isDarkModeOn", true);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor.putBoolean("isDarkModeOn", false);
+                }
+                editor.apply();
+            }
+        });
     }
 }
