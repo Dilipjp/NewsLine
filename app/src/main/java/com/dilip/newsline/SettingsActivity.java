a3a666af781990f1ffbf11aad59b151d82c6c232
Dilipjp <jpdilipk@gmail.com>
2024-08-06 09:43:04 +0530

    Payemt option done

--- app/src/main/java/com/dilip/newsline/SettingsActivity.java
+++ app/src/main/java/com/dilip/newsline/SettingsActivity.java
@@ -8,15 +8,42 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.stripe.android.paymentsheet.PaymentSheetResult;
+import com.stripe.android.PaymentConfiguration;
+import com.stripe.android.paymentsheet.PaymentSheet;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private Switch switchDarkMode;
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
-    private Button btn_back;
+    private Button btn_back, btn_pay;
+   
+    String PublishableKey = "xxx";
+    String SecretKey = "xxx";
+    String CustomerId;
+    String ClientSecret;
+    String EphemeralKey;
+    PaymentSheet paymentSheet;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -26,6 +53,20 @@ public class SettingsActivity extends AppCompatActivity {
 
         switchDarkMode = findViewById(R.id.switch_dark_mode);
         btn_back = findViewById(R.id.btn_back);
+        btn_pay = findViewById(R.id.btn_make_payment);
+
+        btn_pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                paymentFlow();
+            }
+        });
+
+        PaymentConfiguration.init(this, PublishableKey);
+        paymentSheet = new PaymentSheet(this, paymentSheetResult -> {
+            onPaymetResult(paymentSheetResult);
+        });
+        getCustomerId();
 
         btn_back.setOnClickListener(view ->  {
             Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
@@ -61,5 +102,140 @@ public class SettingsActivity extends AppCompatActivity {
                 editor.apply();
             }
         });
+    }
+
+    private void getCustomerId() {
+        StringRequest request = new StringRequest(Request.Method.POST, "https://api.stripe.com/v1/customers",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            CustomerId = object.getString("id");
+                            //Toast.makeText(SettingsActivity.this, "CustomerId:" + CustomerId, Toast.LENGTH_SHORT).show();
+                            getEphemeralKeys();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(SettingsActivity.this, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+                header.put("Authorization", "Bearer "+ SecretKey);
+                return header;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+    }
+
+    private void paymentFlow() {
+        paymentSheet.presentWithPaymentIntent(ClientSecret, new PaymentSheet.Configuration("Dilip", new PaymentSheet.CustomerConfiguration(
+                CustomerId,
+                EphemeralKey
+        )));
+    }
+
+    private void onPaymetResult(PaymentSheetResult paymentSheetResult) {
+        if(paymentSheetResult instanceof PaymentSheetResult.Completed){
+
+            btn_pay.setText("Payment Done");
+            btn_pay.setEnabled(false);
+            Toast.makeText(this, "Payment success", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void getEphemeralKeys() {
+        StringRequest request = new StringRequest(Request.Method.POST, "https://api.stripe.com/v1/ephemeral_keys",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            EphemeralKey = object.getString("id");
+                           // Toast.makeText(SettingsActivity.this, "EphemeralKey:" + EphemeralKey, Toast.LENGTH_SHORT).show();
+                            getClientSecret(CustomerId,EphemeralKey);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(SettingsActivity.this, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+                header.put("Authorization", "Bearer "+ SecretKey);
+                header.put("Stripe-Version" , "2024-06-20");
+                return header;
+            }
+
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("customer",CustomerId);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+    }
+
+    private void getClientSecret(String customerId, String ephemeralKey) {
+        StringRequest request = new StringRequest(Request.Method.POST, "https://api.stripe.com/v1/payment_intents",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            ClientSecret = object.getString("client_secret");
+                           // Toast.makeText(SettingsActivity.this, "ClientSecret:" + ClientSecret, Toast.LENGTH_SHORT).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(SettingsActivity.this, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+                header.put("Authorization", "Bearer "+ SecretKey);
+                return header;
+            }
+
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("customer",CustomerId);
+                params.put("amount","100"+"00");
+                params.put("currency","cad");
+                params.put("automatic_payment_methods[enabled]","true");
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
